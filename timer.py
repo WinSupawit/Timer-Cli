30084d8adbcbe5f0d82a0425531ff70e42e18626
MasterWinZ <supawit.promma6256@gmail.com>
2022-10-02 22:12:04 +0700

    Add argparse

--- timer.py
+++ timer.py
@@ -1,6 +1,7 @@
 import time
 import sys
 import os
+import argparse
 
 def countdown(minute):
     for j in range(minute-1,-1,-1):
@@ -15,4 +16,18 @@ def countdown(minute):
             time.sleep(1)
             sys.stdout.flush()
 
-countdown(1)
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.version = '1.0'
+    parser.add_argument('minute',
+                        action = 'store',
+                        type = int,
+                        choices = range(1, 60),
+                        default = 25,
+                        help = 'Set duration in minute')
+
+    args = parser.parse_args()
+
+    minute = args.minute
+    os.system('cls')
+    countdown(minute)
